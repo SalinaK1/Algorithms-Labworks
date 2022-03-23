27485b5ff52770b93c5cfeef163abc05a63545fc
SalinaK1 <saleena.koirala1@gmail.com>
2022-03-23 13:43:08 +0545

    Logic of code added as comment.

--- Lab2_Sorting/insertion_sort.py
+++ Lab2_Sorting/insertion_sort.py
@@ -1,9 +1,13 @@
+'''
+The insertion sort is like sorting pay cards in our hands.
+'''
+
 def insertion_sort(list_to_sort):
     for j in range(1,len(list_to_sort)):
         key = list_to_sort[j]
         last_sorted = j-1
         while last_sorted>=0 and list_to_sort[last_sorted] > key:
-            list_to_sort[last_sorted +1] = list_to_sort[last_sorted]
+            list_to_sort[last_sorted +1] = list_to_sort[last_sorted]            #Put last sorted key in its correct position
             last_sorted -= 1
-        list_to_sort[last_sorted +1]= key
+        list_to_sort[last_sorted +1]= key                   #Create space for the key to be sorted
 
--- Lab2_Sorting/merge_sort.py
+++ Lab2_Sorting/merge_sort.py
@@ -1,3 +1,9 @@
+'''
+The merge sort is a divide-and-conquer strategy. 
+The problem is divided into equal subparts recursively. {Merge_sort function does that here.}
+Then, the sub-parts are merged maintaining its sorted position (sorted in corect order while merging). {Merge sublist function does that work}
+'''
+
 def merge_sublist(list_to_sort,start,mid,end):
     l1_number = mid-start +1        #includes mid
     l2_number = end-mid
@@ -10,7 +16,7 @@ def merge_sublist(list_to_sort,start,mid,end):
     i=0
     j=0
     for k in range(start,end+1):
-        if i<l1_number and (j >= l2_number or list1[i] <= list2[j]):
+        if i<l1_number and (j >= l2_number or list1[i] <= list2[j]):                    
             list_to_sort[k] = list1[i]
             i +=1
         else: 
@@ -24,3 +30,15 @@ def merge_sort(list_to_sort,start,end):
         merge_sort(list_to_sort,start,mid)
         merge_sort(list_to_sort, mid+1, end)
         merge_sublist(list_to_sort, start,mid,end)
+
+
+
+'''
+Merge-sublist logic:
+i<l1_number and => If list1 is empty all elements of list2 can be copied directly (Directly jump to else)
+j >= l2_number or => if list 2 is empty copy the contents of list 1 
+or list1[i] <= list2[j
+if content of list 1 is lesser then copy the contents of list 1
+
+Order of or is maintained so that list doesn't do out of range for j (if j has exceeded length of list2 copy contents of list1 directly without checking.)
+'''
