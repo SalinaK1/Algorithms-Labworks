0a62d8fac118d577a3c1294f0b000a1dcfbef031
SalinaK1 <saleena.koirala1@gmail.com>
2022-04-13 00:02:08 +0545

    removing a node implemented iteratively.

--- Lab3_BST/bst.py
+++ Lab3_BST/bst.py
@@ -1,6 +1,9 @@
+from platform import node
+
+
 class BinarySearchTree:
     def __init__(self):
-        self.size= 0
+        self._size= 0
         self._root = None
 
     class BST_Node:
@@ -12,7 +15,7 @@ class BinarySearchTree:
             self.parent = None
     
     def size(self):
-        return self.size
+        return self._size
 
     def add(self,key,value):
         node_x = self._root
@@ -32,13 +35,12 @@ class BinarySearchTree:
             node_y.left  = new_node
         else :
             node_y.right  = new_node
-        self.size+=1
+        self._size+=1
       
     def search(self,key):
         nodes = self._root
         while nodes is not None:
             if key == nodes.key:
-                print(f"{key} Found")
                 return nodes.value
             elif key < nodes.key:
                 nodes = nodes.left
@@ -94,20 +96,110 @@ class BinarySearchTree:
         self._postorder_walk(root, output_list)
         return output_list
 
+    def remove(self, key):
+        if self.search(key):
+            nodes = self._root
+            while nodes is not None:        #Traverse upto node to delete
+                if key == nodes.key:
+                    node_to_delete = nodes
+                    break
+                elif key < nodes.key:
+                    nodes = nodes.left
+                else:
+                    nodes = nodes.right
+
+            if node_to_delete.left is None and node_to_delete.right is None:        #delete node with no child
+                if node_to_delete == self._root:
+                    self._root = None
+                else:   
+                    if node_to_delete == node_to_delete.parent.left:
+                        node_to_delete.parent.left = None
+                        node_to_delete.parent = None
+                    else:
+                        node_to_delete.parent.right = None
+                        node_to_delete.parent = None
+
+            elif node_to_delete.left is not None and node_to_delete.right is not None:         #delete node with both child
+                replace_node = node_to_delete.left
+                while replace_node.right is not None:             # find largest key node of left subtree    
+                    replace_node = replace_node.right
+                if node_to_delete == self._root:
+                    self._root = replace_node
+                    node_to_delete.left.parent = replace_node
+                    if node_to_delete != replace_node.parent:
+                        replace_node.parent.right = None
+                    replace_node.parent = None
+                    replace_node.left = node_to_delete.left
+                    replace_node.right = node_to_delete.right
+                    node_to_delete.left = None
+                else:
+                    if node_to_delete == node_to_delete.parent.left:        #node to delete is left child
+                        node_to_delete.parent.left = replace_node
+                        if node_to_delete != replace_node.parent:
+                            replace_node.parent.right = None
+                        replace_node.parent = node_to_delete.parent
+                        node_to_delete.left.parent = replace_node
+                        if replace_node.left is not None:
+                            replace_node.left = node_to_delete.left
+                        replace_node.right = node_to_delete.right
+                        node_to_delete.left = None
+                        replace_node.right.parent = replace_node
+                    else:                                                   #node to delete is left child
+                        node_to_delete.parent.right = replace_node
+                        if node_to_delete != replace_node.parent:
+                            replace_node.parent.right = None
+                        replace_node.parent = node_to_delete.parent
+                        node_to_delete.left.parent = replace_node
+                        if replace_node.left is not None:
+                            replace_node.left = node_to_delete.left
+                        replace_node.right = node_to_delete.right
+                        node_to_delete.left = None
+                        replace_node.right.parent = replace_node
+                
+            elif node_to_delete.left is not None:           #delete node with left child only
+                if node_to_delete == self._root:
+                    self._root = node_to_delete.left
+                    node_to_delete.left.parent = None
+                    node_to_delete.left = None
+                else:
+                    if node_to_delete == node_to_delete.parent.left:        #node to delete is left child
+                        node_to_delete.parent.left = node_to_delete.left
+                        node_to_delete.left.parent = node_to_delete.parent 
+                    else:
+                        node_to_delete.parent.right = node_to_delete.left
+                        node_to_delete.left.parent = node_to_delete.parent 
+
+            else:                                               #delete node with right child only
+                if node_to_delete == self._root:
+                    self._root = node_to_delete.right
+                    node_to_delete.right.parent = None
+                    node_to_delete.right = None
+                else:
+                    if node_to_delete == node_to_delete.parent.left:        #node to delete is right child
+                        node_to_delete.parent.left = node_to_delete.right
+                        node_to_delete.right.parent = node_to_delete.parent 
+                    else:
+                        node_to_delete.parent.right = node_to_delete.right
+                        node_to_delete.right.parent = node_to_delete.parent  
+            self._size-=1  
+
+        else:                       #key to delete is not present in BST
+            return False
+
 
 
 if __name__ == '__main__':
     bst = BinarySearchTree()
     bst.add(5,"Five")
-    print(bst.size)
+    print(bst._size)
     bst.add(6,"Six")
-    print(bst.size)
+    print(bst._size)
     bst.add(8,"Eight")
-    print(bst.size)
+    print(bst._size)
     bst.add(2,"Two")
-    print(bst.size)
+    print(bst._size)
     bst.add(4,"Four")
-    print(bst.size)
+    print(bst._size)
     print(bst.search(5))
     print(bst.search(6))
     print(bst.search(8))
